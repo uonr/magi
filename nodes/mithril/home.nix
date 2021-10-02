003a8c6f48a8d778034739e5656c8d708bc6aa23
Tachibana Kiyomi <me@yuru.me>
2021-10-03 06:08:49 +0900

    forward gpg agent

--- nodes/mithril/home.nix
+++ nodes/mithril/home.nix
@@ -34,6 +34,13 @@ in {
       koma = {
         hostname = "10.110.100.2";
         user = "mikan";
+        extraOptions = let 
+          gpgHome = "/Users/mikan/.gnupg";
+          target = "/run/user/1000/gnupg";
+        in {
+          "RemoteForward ${target}/S.gpg-agent" = "${gpgHome}/S.gpg-agent";
+          "RemoteForward ${target}/S.gpg-agent.ssh" = "${gpgHome}/S.gpg-agent.ssh";
+        };
       };
 
       "*" = {
