23195960a7c1401d499c6170a6576c8941fc1491
Tachibana Kiyomi <me@yuru.me>
2021-10-04 02:43:02 +0900

    enlarge max upload size

--- nodes/koma/manga.nix
+++ nodes/koma/manga.nix
@@ -21,7 +21,7 @@ in {
     forceSSL = true;
     locations."/" = {
       proxyPass = "http://127.0.0.1:${port}";
-      extraConfig = ''client_max_body_size 1024m;'';
+      extraConfig = ''client_max_body_size 2048m;'';
       proxyWebsockets = true;
     };
   };
