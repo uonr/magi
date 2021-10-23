2d1fa539f30db7d47b9874302b1451f9d13e3f69
Tachibana Kiyomi <me@yuru.me>
2021-10-23 22:45:20 +0900

    zsh history settings

--- share/init.zsh
+++ share/init.zsh
@@ -5,3 +5,12 @@ prompt pure
 autoload -z edit-command-line
 zle -N edit-command-line
 bindkey "^X^E" edit-command-line
+
+## History command configuration
+# source: https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/history.zsh
+setopt extended_history       # record timestamp of command in HISTFILE
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt share_history          # share command history data
